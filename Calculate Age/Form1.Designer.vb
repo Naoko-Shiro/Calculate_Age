74de21de740fc2364d696731e9669091ef9e03cb
Nairah Saldana <43283099+Seno-S@users.noreply.github.com>
2018-09-25 06:17:30 -0500

    Add files via upload

---
+++ Calculate Age/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.numYear = New System.Windows.Forms.NumericUpDown()
+        Me.cmbMonth = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.numYear, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'numYear
+        '
+        Me.numYear.Location = New System.Drawing.Point(54, 181)
+        Me.numYear.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.numYear.Maximum = New Decimal(New Integer() {2016, 0, 0, 0})
+        Me.numYear.Minimum = New Decimal(New Integer() {1900, 0, 0, 0})
+        Me.numYear.Name = "numYear"
+        Me.numYear.Size = New System.Drawing.Size(180, 29)
+        Me.numYear.TabIndex = 0
+        Me.numYear.Value = New Decimal(New Integer() {1900, 0, 0, 0})
+        '
+        'cmbMonth
+        '
+        Me.cmbMonth.FormattingEnabled = True
+        Me.cmbMonth.Items.AddRange(New Object() {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"})
+        Me.cmbMonth.Location = New System.Drawing.Point(314, 179)
+        Me.cmbMonth.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.cmbMonth.Name = "cmbMonth"
+        Me.cmbMonth.Size = New System.Drawing.Size(180, 29)
+        Me.cmbMonth.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 155)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 21)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Birth Year"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(309, 153)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(93, 21)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Birth Month"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(219, 262)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(112, 37)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(578, 416)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmbMonth)
+        Me.Controls.Add(Me.numYear)
+        Me.Cursor = System.Windows.Forms.Cursors.Cross
+        Me.Font = New System.Drawing.Font("Nirmala UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.Navy
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Age Calculator"
+        CType(Me.numYear, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents numYear As System.Windows.Forms.NumericUpDown
+    Friend WithEvents cmbMonth As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Calculate Age/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' How to make (declare) a variable '
+        Dim firstName As String ' String is the variable type for text '
+        ' assign it a value
+        firstName = "Nairah"
+
+        ' You can declare more than one variable on one line '
+        Dim year, month As Integer ' Integers are pos/neg whole numbers '
+
+        ' initialize variables '
+        year = 2000
+        month = 1
+
+        ' you can declare and initialize all in one line
+        Dim yearsOld As Integer = 0
+
+        firstName = InputBox("Enter your name:")
+
+        year = numYear.Value
+        month = cmbMonth.SelectedIndex + 1
+
+        MessageBox.Show(firstName & " was born in  " & month & " of " & year)
+
+        yearsOld = Date.Today.Year - year
+
+        'check if birth month is later than current month
+        If Date.Today.Month <= month Then
+            yearsOld = yearsOld - 1 ' decrease yearsOld by 1
+        End If
+
+        MessageBox.Show(firstName & " is " & yearsOld & " years old")
+
+
+
+    End Sub
+End Class
